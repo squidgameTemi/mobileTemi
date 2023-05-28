3bcc97f1221bcf201863c79fd40491a4cab4bf88
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2023-05-28 15:01:21 +0900

    Add button feature
    Add start button feature & end button feature

--- app/src/main/java/com/example/mobiletemi/MainActivity.java
+++ app/src/main/java/com/example/mobiletemi/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mobiletemi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databaseReference.child("game").child("start").setValue(0);
+        databaseReference.child("game").child("end").setValue(0);
+    }
+
+    public void game_start(View view){
+        databaseReference.child("game").child("start").setValue(1);
+    }
+
+    public void game_end(View view){
+        databaseReference.child("game").child("end").setValue(1);
     }
 }
