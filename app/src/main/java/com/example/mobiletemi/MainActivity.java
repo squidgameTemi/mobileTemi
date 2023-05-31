5bc44c7b325de26cafbf41ade153b0d2cc36c591
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2023-05-31 21:28:31 +0900

    Add level

--- app/src/main/java/com/example/mobiletemi/MainActivity.java
+++ app/src/main/java/com/example/mobiletemi/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,19 +15,36 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    private RadioButton rb_easy, rb_normal, rb_hard;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        databaseReference.child("game").child("start").setValue(0);
-        databaseReference.child("game").child("end").setValue(0);
+
+        rb_easy = (RadioButton)findViewById(R.id.easy);
+        rb_normal = (RadioButton)findViewById(R.id.normal);
+        rb_hard = (RadioButton)findViewById(R.id.hard);
+
+        databaseReference.child("game").setValue(-1);
+
     }
 
     public void game_start(View view){
-        databaseReference.child("game").child("start").setValue(1);
+        if(rb_easy.isChecked()){
+            databaseReference.child("game").setValue(1);
+        }
+        else if(rb_normal.isChecked()){
+            databaseReference.child("game").setValue(2);
+        }
+        else if(rb_hard.isChecked()){
+            databaseReference.child("game").setValue(3);
+        }
     }
 
     public void game_end(View view){
-        databaseReference.child("game").child("end").setValue(1);
+        databaseReference.child("game").setValue(0);
     }
 }
